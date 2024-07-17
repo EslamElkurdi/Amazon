d0234a0979d3ad718e2cb5c7b0cdd931f0e8c7d6
Eslam Gamal <elkurdieslam@gmail.com>
2024-07-17 20:22:08 +0300

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app = express();
+const { resolve } = require("path");
+const env = require("dotenv").config({ path: "./.env" });
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2022-08-01",
+});
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+app.use(express.static(process.env.STATIC_DIR));
+
+app.get("/", (req, res) => {
+  // write here
+  const path = resolve(process.env.STATIC_DIR + "/index.html");
+  res.sendFile(path);
+});
+
+app.get("/config", (req, res) => {
+  res.send({
+    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  });
+});
+
+app.post("/create-payment-intent", async (req, res) => {
+  try {
+    console.log("body: ",req.body);
+    console.log("Payment intent created!");
+    
+    const paymentIntent = await stripe.paymentIntents.create({
+      currency: "EUR",
+      amount: req.body.amount,
+      automatic_payment_methods: { enabled: true },
+    });
+
+    // Send publishable key and PaymentIntent details to client
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (e) {
+    return res.status(400).send({
+      error: {
+        message: e.message,
+      },
+    });
+  }
+});
+
+app.listen(5252, () =>
+  console.log(`Node server listening at http://localhost:5252`)
+);
